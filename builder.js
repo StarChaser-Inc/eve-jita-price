5a676a2168b38e24be8f82bf6a2e96c00a4ba7c4
Alex Chen <M.AC.AlexChen@outlook.com>
2024-10-25 20:47:43 +0800

    none

---
+++ builder.js
@@ -0,0 +1,28 @@
+'use strict'
+const yaml = require('js-yaml')
+const fs = require('fs')
+const path = require('path')
+const zlib = require('zlib')
+
+const filePath = path.join(__dirname, 'types.yaml')
+const content = fs.readFileSync(filePath, 'utf8')
+const data = yaml.load(content)
+const newFile = []
+for (const key in data) {
+  if (Object.prototype.hasOwnProperty.call(data, key)) {
+    if (!data[key].marketGroupID) continue
+    newFile.push({
+      id: key,
+      name: data[key].name,
+      groupID: data[key].group
+    })
+  }
+}
+const newFileContent = JSON.stringify(newFile)
+const newFilePath = path.join(__dirname, 'types.json')
+fs.writeFileSync(newFilePath, newFileContent)
+const readStream = fs.createReadStream(path.join(__dirname, 'types.json'))
+const writeStream = fs.createWriteStream(path.join(__dirname, 'types.json.gz'))
+const gzip = zlib.createGzip()
+readStream.pipe(gzip).pipe(writeStream)
+writeStream.on('finish', () => fs.unlinkSync(path.join(__dirname, 'types.json')))
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import { Context, Schema, h } from 'koishi'
-import * as fs from 'fs'
-import * as zlib from 'zlib'
-import * as path from 'path'
+import { Context, Schema } from "koishi"
+import fs from "fs"
+import * as zlib from "zlib"
+import filePath from "path"
 
-export const name = 'eve-jita-price'
+export const name = "eve-jita-price"
 
-interface json {
+interface itemInfo {
   id: number
   name: {
     de: string
@@ -21,122 +21,117 @@ interface json {
 
 export interface Config {
   maxSearch: number
-  markdown: boolean
+  preDecompression: boolean
+  customSpecialFields: {
+    monitoringContent: string
+    response: string
+    directOutput: boolean
+  }[]
+  customPriceInquiryInstructionsAndLocation: {
+    command: string
+    location: string
+  }[]
+  customTypesJsonFilePath: string
 }
 
 export const Config: Schema<Config> = Schema.object({
   maxSearch: Schema.number().description('最大搜索结果数量').default(10),
-  markdown: Schema.boolean().description('是否使用 Markdown 格式输出').default(false)
-})
+  customSpecialFields: Schema.array(Schema.object({
+    monitoringContent: Schema.string().description('监听内容').required(),
+    response: Schema.string().description('响应内容').required().role('textarea'),
+    directOutput: Schema.boolean().description('是否直接输出响应内容（值为false时会在输出后执行查价计算）').default(true)
+  })).description('自定义特殊响应字段（因为群友特爱整活，所以增加了这个功能）').role('table'),
+  preDecompression: Schema.boolean().description('是否在插件启动时解压typeId文件').default(true),
+  customPriceInquiryInstructionsAndLocation: Schema.array(Schema.object({
+    command: Schema.string().description('指令').required(),
+    location: Schema.string().description('星域id').required()
+  })).description('自定义价格询价指令及星域id').role('table').default([{command: 'jita', location: '10000002'}]),
+  customTypesJsonFilePath: Schema.string().description('自定义types.json.gz文件路径').default(`${filePath.join(__dirname, 'types.json.gz')}`)
+}) as Schema<Config>
 
+export function apply(ctx: Context, cfg: Config): void {
+  function formatNumberWithCommas(num: number | string): string {
+    if (typeof num === 'number' && isNaN(num)) return 'NaN'
+    typeof num === 'string' ? num = +parseFloat(num).toFixed(2) : num = +num.toFixed(2)
+    const [integer, fraction] = num.toString().split(".")
+    const formattedInteger = integer.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+    return fraction ? `${formattedInteger}.${fraction}` : formattedInteger
+  }
 
-function formatNumberWithCommas(num: number | string): string {
-  const [integer, fraction] = num.toString().split(".")
-  const formattedInteger = integer.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-  return fraction ? `${formattedInteger}.${fraction}` : formattedInteger
-}
+  function search(name: string, types: itemInfo[]): itemInfo[] {
+    return types.filter(item =>
+      (item.name && item.name.zh && item.name.zh.startsWith(name)) ||
+      (item.name && item.name.en && item.name.en.toUpperCase().startsWith(name.toUpperCase())) ||
+      (item.name && item.name.de && item.name.de.startsWith(name)) ||
+      (item.name && item.name.fr && item.name.fr.startsWith(name)) ||
+      (item.name && item.name.es && item.name.es.startsWith(name)) ||
+      (item.name && item.name.ja && item.name.ja.startsWith(name)) ||
+      (item.name && item.name.ru && item.name.ru.startsWith(name))
+    )
+  }
 
-export function apply(ctx: Context, cfg: Config) {
-  let typeIDs: json[]
-  ctx.on('ready', () => {
-    const typeIDs_json_gz = fs.readFileSync(path.resolve(__dirname, 'types.json.gz'))
+  async function getItemPrice(itemId: number, locationId: string, method: 'buy' |'sell'): Promise<number[]> {
     try {
-      const buffer = zlib.gunzipSync(typeIDs_json_gz)
-      typeIDs = JSON.parse(buffer.toString()) as json[]
-    } catch (err) {
-      ctx.logger.error(err)
-      ctx.stop()
+      return ((await ctx.http.get(`https://esi.evetech.net/latest/markets/${locationId}/orders/?datasource=tranquility&order_type=${method}&type_id=${itemId}`)) as { price: number }[]).map(order => order.price)
+    } catch {
+      return [NaN]
+    }
+  }
+
+  function gunzip(): itemInfo[] {
+    return JSON.parse(zlib.gunzipSync(fs.readFileSync(cfg.customTypesJsonFilePath)).toString())
+  }
+
+  let types: itemInfo[] = undefined
+  ctx.on('ready', () => {
+    if (cfg.preDecompression) {
+      types = gunzip()
     }
   })
-  ctx.command('jita <...inputname>')
-  .action(async ({session}, ...inputname) => {
-    const name = inputname.join(' ')
-    if (!name){
-      return '请输入要查询的物品名称'
-    } else {
-      let search = typeIDs.filter(item =>
-        (item.name && item.name.zh && item.name.zh.startsWith(name)) ||
-        (item.name && item.name.en && item.name.en.toUpperCase().startsWith(name.toUpperCase())) ||
-        (item.name && item.name.de && item.name.de.startsWith(name)) ||
-        (item.name && item.name.fr && item.name.fr.startsWith(name)) ||
-        (item.name && item.name.es && item.name.es.startsWith(name)) ||
-        (item.name && item.name.ja && item.name.ja.startsWith(name)) ||
-        (item.name && item.name.ru && item.name.ru.startsWith(name))
-      )
-      if (search.length === 0){
-        return '未找到相关物品'
+  for (const { command, location } of cfg.customPriceInquiryInstructionsAndLocation) {
+    ctx.command(`${command} <...itemName>`)
+    .action(async (_, ...itemName) => {
+      const name = itemName.join(' ')
+      if (!name) return '请输入物品名称'
+      if (cfg.customSpecialFields.map(field => field.monitoringContent).includes(name)) {
+        if (cfg.customSpecialFields.find(field => field.monitoringContent === name).directOutput) return cfg.customSpecialFields.find(field => field.monitoringContent === name).response
+        else _.session.send(cfg.customSpecialFields.find(field => field.monitoringContent === name).response)
       }
-      if (!name.includes('SKIN') || !name.includes('涂装')){
-        search = search.filter(item => (item.name && item.name.en && !item.name.en.includes('SKIN')))
-      }
-      if (!name.includes('Blueprint') || !name.includes('蓝图')){
-        search = search.filter(item => (item.name && item.name.en && !item.name.en.includes('Blueprint')))
-      }
-      if (search.length === 0){
-        search = typeIDs.filter(item =>
-          (item.name && item.name.zh && item.name.zh.startsWith(name)) ||
-          (item.name && item.name.en && item.name.en.toUpperCase().startsWith(name.toUpperCase())) ||
-          (item.name && item.name.de && item.name.de.startsWith(name)) ||
-          (item.name && item.name.fr && item.name.fr.startsWith(name)) ||
-          (item.name && item.name.es && item.name.es.startsWith(name)) ||
-          (item.name && item.name.ja && item.name.ja.startsWith(name)) ||
-          (item.name && item.name.ru && item.name.ru.startsWith(name))
-        )
-      }
+      let uTypes: itemInfo[] = []
+      if (types) uTypes = types
+      else uTypes = gunzip()
+      let items = search(name, uTypes)
+      if (items.length === 0) return `没有找到名称为 "${name}" 的物品`
+      if (!name.includes('SKIN') || !name.includes('涂装')) items = items.filter(item => item.name && item.name.en && !item.name.en.includes('SKIN'))
+      if (!name.includes('Blueprint') || !name.includes('蓝图')) items = items.filter(item => item.name && item.name.en && !item.name.en.includes('Blueprint'))
+      if (items.length === 0) items = search(name, uTypes)
+      if (items.length > cfg.maxSearch) return `找到 ${items.length} 个结果，请缩小搜索范围`
       let suit = false
       let plex = false
-      let ALL_SELL = 0
-      let ALL_BUY = 0
-      let ALL_MPRICE = 0
-      if (search.every(item => item.groupID === 300) && search.length === 6){
-        suit = true
-      }
-      if (search.every(item => item.id === 44992) && search.length === 1){
-        plex = true
-      }
-      const price = []
-      for (let i = 0; i < search.length; i++){
-        if (i + 1 >= cfg.maxSearch) break
-        const buy = (await ctx.http.get(`https://esi.evetech.net/latest/markets/10000002/orders/?datasource=tranquility&order_type=buy&type_id=${search[i].id}`)).map(order => order.price)
-        const sell = (await ctx.http.get(`https://esi.evetech.net/latest/markets/10000002/orders/?datasource=tranquility&order_type=sell&type_id=${search[i].id}`)).map(order => order.price)
-        const maxbuy = Math.max(...buy)
-        const minsell = Math.min(...sell)
-        const mprice = (maxbuy + minsell) / 2
-        if (cfg.markdown){
-          price.push(`## ${search[i].name.zh}/${search[i].name.en}&#10;- 最低卖价：${formatNumberWithCommas(minsell)} ISK&#10;- 最高买价：${formatNumberWithCommas(maxbuy)} ISK&#10;- 平均价格：${formatNumberWithCommas(mprice)} ISK`)
-        } else {
-          price.push(`${search[i].name.zh}/${search[i].name.en}&#10;  -最低卖价：${formatNumberWithCommas(minsell)} ISK&#10;  -最高买价：${formatNumberWithCommas(maxbuy)} ISK&#10;  -平均价格：${formatNumberWithCommas(mprice)} ISK`)
+      let allSell = 0
+      let allBuy = 0
+      let allM = 0
+      if (items.every(i => i.groupID === 300) && items.length === 6) suit = true
+      if (items.every(i => i.id === 44992) && items.length === 1) plex = true
+      let mArr = []
+      for (const item of items) {
+        const maxBuy = Math.max(...(await getItemPrice(item.id, location, 'buy')))
+        const minSell = Math.min(...(await getItemPrice(item.id, location,'sell')))
+        const m = +((maxBuy + minSell) / 2).toFixed(2)
+        if (suit || plex) {
+          allBuy += maxBuy
+          allSell += minSell
+          allM += m
         }
-        if (suit){
-          ALL_SELL += minsell
-          ALL_BUY  += maxbuy
-          ALL_MPRICE += mprice
-        }
-        if (plex){
-          ALL_SELL += minsell
-          ALL_BUY  += maxbuy
-          ALL_MPRICE += mprice
-        }
+        mArr.push(`${item.name.zh}/${item.name.en}\n  -最低卖价：${formatNumberWithCommas(minSell)} ISK\n  -平均价格：${formatNumberWithCommas(m)} ISK\n  -最高买价：${formatNumberWithCommas(maxBuy)} ISK`)
       }
-      if (suit){
-        if (cfg.markdown){
-          price.push(`## 以上物品总价&#10;- 最低卖价：${formatNumberWithCommas(ALL_SELL)} ISK&#10;- 最高买价：${formatNumberWithCommas(ALL_BUY)} ISK&#10;- 平均价格：${formatNumberWithCommas(ALL_MPRICE)} ISK`)
-        } else {
-          price.push(`以上物品总价：&#10;  -最低卖价：${formatNumberWithCommas(ALL_SELL)} ISK&#10;  -最高买价：${formatNumberWithCommas(ALL_BUY)} ISK&#10;  -平均价格：${formatNumberWithCommas(ALL_MPRICE)} ISK`)
-        }
+      if (suit) {
+        mArr.push(`以上物品总价\n  -最低卖价：${formatNumberWithCommas(allSell)} ISK\n  -平均价格：${formatNumberWithCommas(allM)} ISK\n  -最高买价：${formatNumberWithCommas(allBuy)} ISK\n`)
       }
-      if (plex){
-        if (cfg.markdown){
-          price.push(`## 500x PLEX&#10;- 最低卖价：${formatNumberWithCommas(ALL_SELL * 500)} ISK&#10;- 最高买价：${formatNumberWithCommas(ALL_BUY * 500)} ISK&#10;- 平均价格：${formatNumberWithCommas(ALL_MPRICE * 500)} ISK`)
-        } else {
-          price.push(`500x PLEX&#10;  -最低卖价：${formatNumberWithCommas(ALL_SELL * 500)} ISK&#10;  -最高买价：${formatNumberWithCommas(ALL_BUY * 500)} ISK&#10;  -平均价格：${formatNumberWithCommas(ALL_MPRICE * 500)} ISK`)
-        }
+      if (plex) {
+        mArr.push(`500x PLEX\n  -最低卖价：${formatNumberWithCommas(allSell * 500)} ISK\n  -平均价格：${formatNumberWithCommas(allM * 500)} ISK\n  -最高买价：${formatNumberWithCommas(allBuy * 500)} ISK\n`)
       }
-      if (cfg.markdown){
-        return h('at', { id: session.userId }) + '.&#10;' + price.join('&#10;&#10;---&#10;')
-      } else {
-        return h('at', { id: session.userId }) + '.&#10;' + price.join('&#10;--------------------&#10;')
-      }
-    }
-  })
+      return mArr.join('\n--------------\n')
+    })
+  }
 }
